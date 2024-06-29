d16d8fd327902381beb6c5c01a5dd0c8ad3d0066
jubairsiyum <jubairsiyum@gmail.com>
2024-06-29 21:24:30 +0600

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+   function fun(){
+    return "<h1>Hello, Laravel!</h1>";
+   }
+}
